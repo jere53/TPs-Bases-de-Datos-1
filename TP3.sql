4b6ce9098ca6f1273d968f6a1c899e39b3e7ffe0
jere53 <53.salinas.jeremias@gmail.com>
2020-10-02 20:23:42 -0300

    segunda parte del TP3, salvo 5e

--- TP3.sql
+++ TP3.sql
@@ -142,3 +142,342 @@ UPDATE trabaja_en SET id_proy = 1 WHERE id_proy = 3;
 --B.6 No nos va a dejar por la RIR con auspicia. No modifica nada.
 UPDATE proyecto SET id_proy = 5 WHERE id_proy = 2;
 
+--NOTA: Para el C, las tablas tienen otra forna a como las maneja el ejercicio,
+--por eso el orden de los VALUES en la insercion cambia.
+--C.i (Match Simple, acepta si al menos una columna en la referenciante es NULL)
+--Match Simple es la opcion por defecto en postgresql, no hay que modificar la FK
+--1 la va a aceptar porque una columna es NULL
+INSERT INTO auspicio VALUES ('Dell',1, 'B', null);
+
+--2 la va a aceptar porque un valor es null
+INSERT INTO auspicio VALUES ('Oracle', 3, null, null);
+
+--3
+SELECT * FROM empleado; --para ver que valores debe satisfacer
+--la va a rechazar porque no hay un empleado de tipo A con numero 3
+INSERT INTO auspicio VALUES ('Google', 5, 'A', 3);
+
+--4 la va a aceptar porque un atributo es null
+INSERT INTO auspicio VALUES ('HP', 1, null, 3);
+
+--C.ii (Match Parcial, los valores no null se corresponden con una tupla referenciada)
+--Postgresql no soporta match parcial, no lo podemos probar :(
+SELECT * FROM empleado; --para ver que debe satisfacer
+--1 la va a aceptar porque hay un empleado con tipo B
+
+--2 la va a aceptar porque todos los valores de la FK son null
+
+--3 la va a rechazar porque no hay un empleado con numero 3 y tipo A
+
+--4 la va a rechazar porque no hay un empleado con numero 3
+
+--C.iii (Match Full, acepta si todos los valores que referencian son null)
+--modificamos la FK para probar
+ALTER TABLE auspicio DROP CONSTRAINT fk_auspicio_a_empleado;
+ALTER TABLE auspicio
+    ADD CONSTRAINT fk_auspicio_a_empleado
+    FOREIGN KEY (tipo_e, nro_e)
+    REFERENCES empleado (tipo_e, nro_e)
+    MATCH FULL
+    ON DELETE SET NULL
+    ON UPDATE RESTRICT;
+SELECT * FROM empleado; --para ver que debe satisfacer
+--1 la va a rechazar porque mezcla valores que referencian no null y null
+INSERT INTO auspicio VALUES ('Dell',1, 'B', null);
+
+--2 la va a aceptar porque los dos valores que referencian son null
+INSERT INTO auspicio VALUES ('Oracle', 3, null, null);
+
+--3 la va a rechazar porque no hay una tupla con numero 3 y tipo A en empleado
+INSERT INTO auspicio VALUES ('Google', 5, 'A', 3);
+
+--4 la va a rechazar porque mezcla valores que referencian no null y null
+INSERT INTO auspicio VALUES ('HP', 1, null, 3);
+
+-- Ejercicio 2
+--Creamos el esquema
+CREATE TABLE cliente (
+    zona char(1) NOT NULL ,
+    nro_c integer NOT NULL ,
+    nombre varchar(30) NOT NULL ,
+    ciudad char(2) NOT NULL ,
+    CONSTRAINT pk_cliente PRIMARY KEY (zona, nro_c)
+);
+
+CREATE TABLE instalacion (
+    zona char(1) NOT NULL ,
+    nro_c integer NOT NULL ,
+    id_serv char(2) NOT NULL ,
+    anio integer NOT NULL ,
+    mes integer NOT NULL ,
+    cant_horas integer NOT NULL ,
+    tarea char(2) NOT NULL ,
+    CONSTRAINT pk_instalacion PRIMARY KEY (zona,nro_c,id_serv,anio,mes)
+);
+
+CREATE TABLE servicio (
+    id_serv char(2) NOT NULL ,
+    nombre_serv varchar(10) NOT NULL ,
+    anio_comienzo integer NOT NULL ,
+    anio_fin integer NULL ,
+    CONSTRAINT pk_servicio PRIMARY KEY (id_serv)
+);
+
+CREATE TABLE referecnia (
+    id_serv char(2) NOT NULL ,
+    motivo varchar(15) NOT NULL ,
+    zona char(1) NULL ,
+    nro_c integer NULL ,
+    CONSTRAINT pk_referencia PRIMARY KEY (id_serv, motivo)
+);
+
+ALTER TABLE instalacion
+ADD CONSTRAINT fk_instalacion_cliente
+FOREIGN KEY (zona, nro_c) REFERENCES cliente (zona, nro_c)
+ON DELETE CASCADE
+ON UPDATE RESTRICT;
+
+ALTER TABLE instalacion
+ADD CONSTRAINT fk_instalacion_servicio
+FOREIGN KEY (id_serv) REFERENCES servicio (id_serv)
+ON DELETE RESTRICT
+ON UPDATE RESTRICT;
+
+ALTER TABLE referecnia
+ADD CONSTRAINT fk_referencia_servicio
+FOREIGN KEY (id_serv) REFERENCES servicio (id_serv)
+ON DELETE RESTRICT
+ON UPDATE CASCADE;
+
+ALTER TABLE referecnia
+ADD CONSTRAINT fk_referencia_cliente
+FOREIGN KEY (zona, nro_c) REFERENCES cliente (zona, nro_c)
+ON DELETE RESTRICT
+ON UPDATE SET NULL;
+
+INSERT INTO cliente
+VALUES ('A', 1, 'Juan Ro', 'C1'),
+       ('A', 2, 'Alberto Efe', 'C1'),
+       ('B', 1, 'Esteban Hache', 'C1'),
+       ('C', 2, 'Jose Ge', 'C3'),
+       ('D', 3, 'Luis Ene', 'C2');
+
+SELECT * FROM cliente;
+
+INSERT INTO servicio
+VALUES ('S1', 'Serv 1', 2010, 2012),
+       ('S2', 'Serv 2', 2012, 2012),
+       ('S3', 'Serv 3', 2009, NULL);
+
+SELECT * FROM servicio;
+
+INSERT INTO instalacion
+VALUES ('A', 1, 'S1', 5, 2011, 5, 'T1'),
+       ('B', 1, 'S2', 5, 2012, 7, 'T1'),
+       ('C', 2, 'S1', 4, 2010, 9, 'T2'),
+       ('A', 2, 'S3', 8, 2009, 6, 'T2');
+
+SELECT * FROM instalacion;
+
+INSERT INTO referecnia
+VALUES ('S1', 'Puntualidad', 'D', 3),
+       ('S2', 'Calidad inst.', 'C', 2),
+       ('S3', 'Costo', 'C', 2),
+       ('S1', 'Atencion', 'D', 3);
+
+SELECT * FROM referecnia;
+
+--a)
+--a.1
+DELETE FROM Cliente WHERE nro_c = 1;
+--Quedan las tuplas con Alberto F, Jose G y Luis N en cliente
+-- Se eliminan las 2 tuplas de Instalacion con nro cliente 1, quedan las de nro 2.
+SELECT * FROM cliente;
+SELECT * FROM instalacion;
+
+--a.2
+UPDATE instalacion SET id_serv = 'S5' WHERE id_serv = 'S2';
+--No hace nada porque no hay tuplas con S2  en instalacion, se borraron en lo anterior
+SELECT * FROM instalacion;
+
+--a.3
+UPDATE cliente SET zona = 'Z' WHERE zona = 'D';
+--cambia la zona de Luis N (nro_c 3) a Z
+--en referencia, la zona y nro_c de las tuplas con S1 quedan en NULL
+--no hay cambios en instalacion porque el cliente modificado no esta en instalacion
+SELECT * FROM referecnia;
+SELECT * FROM cliente;
+SELECT * FROM instalacion;
+
+--b con match simple
+
+INSERT INTO referecnia VALUES ('S4', 'Prueba', null, 8);
+--se rechaza por la fk con servicio, S4 no esta en serv
+
+INSERT INTO referecnia VALUES (null, 'Prueba', null, 8);
+--se rechaza porque el id serv no puede ser null
+
+INSERT INTO referecnia VALUES ('S3', 'Prueba', null, 8);
+--se acepta porque satisface la rir de servivcio y en la otra hay un referenciante nulo
+
+INSERT INTO referecnia VALUES ('S3', 'Prueba2', null, null);
+--se acepta porque satisface la rir de servivcio y en la otra hay un referenciante nulo
+
+SELECT * FROM referecnia;
+
+--b con match parcial (no se puede probar, match parcial no anda en postgresql)
+
+INSERT INTO referecnia VALUES ('S4', 'Prueba', null, 8);
+--se rechaza por la fk con servicio, S4 no esta en servicio
+
+INSERT INTO referecnia VALUES (null, 'Prueba', null, 8);
+--se rechaza porque el id serv no puede ser null
+
+INSERT INTO referecnia VALUES ('S3', 'Prueba', null, 8);
+--se rechaza porque 8 no es un numero de cliente que este en cliente
+
+INSERT INTO referecnia VALUES ('S3', 'Prueba2', null, null);
+--se acepta porque satisface la rir de servivcio y en la otra son todos nulos
+
+--b con match full
+--modifs para probar
+DELETE FROM referecnia WHERE motivo LIKE 'Prueb%';
+
+ALTER TABLE referecnia DROP CONSTRAINT fk_referencia_cliente;
+
+ALTER TABLE referecnia
+ADD CONSTRAINT fk_referencia_cliente
+FOREIGN KEY (zona, nro_c) REFERENCES cliente (zona, nro_c)
+    MATCH FULL
+ON DELETE RESTRICT
+ON UPDATE SET NULL;
+
+INSERT INTO referecnia VALUES ('S4', 'Prueba', null, 8);
+--se rechaza por la fk con servicio, S4 no esta en servicio
+
+INSERT INTO referecnia VALUES (null, 'Prueba', null, 8);
+--se rechaza porque el id serv no puede ser null
+
+INSERT INTO referecnia VALUES ('S3', 'Prueba', null, 8);
+--se rechaza porque no se pueden mezclar nulos y no nulos en los referenciantes
+
+INSERT INTO referecnia VALUES ('S3', 'Prueba2', null, null);
+--se acepta porque satisface la rir de servivcio y en la otra son todos nulos
+
+SELECT * FROM referecnia;
+
+-- Ejercicio 3
+-- No estan armados los esquemas para probar, ain't nobody got time for that.
+
+--a
+CREATE DOMAIN Nacionalidad_valida
+AS varchar(20) NOT NULL
+CHECK ( VALUE = 'Argentina' OR VALUE = 'Espaniol' OR VALUE = 'Inglesa'
+        OR VALUE = 'Alemana' OR VALUE = 'Chilena');
+
+--b
+CREATE DOMAIN Fecha_valida
+AS date NOT NULL
+CHECK ( extract(year from VALUE) >= 2010 );
+
+--c
+ALTER TABLE articulo
+    ADD CHECK (((extract(year from fecha_pub) > 2017) AND (nacionalidad = 'Argentina'))
+        OR (extract(year from fecha_pub) < 2017));
+
+--d
+ALTER TABLE contiene
+    ADD CHECK ( (SELECT count(id_articulo)) <= 15 );
+
+--e POSTGRESQL NO SOPORTA ASSERTION :(
+CREATE ASSERTION assertion_e CHECK ((SELECT count(SELECT id_articulo
+                                                    FROM articulo
+                                                    WHERE nacionalidad = 'Argentina')
+                                        FROM contiene) <= 10)
+
+-- Ejercicio 4
+
+--a
+ALTER TABLE provee ADD CHECK ( (SELECT count(id_prov)) <= 20 )
+
+--b
+CREATE DOMAIN sucursal_valida
+AS varchar(20) NOT NULL
+CHECK ( VALUE LIKE 'S_%');
+
+--c
+ALTER TABLE producto
+    ADD CHECK (
+        ((presentacion IS NULL) AND (descripcion IS NOT NULL))
+        OR ((presentacion IS NOT NULL) AND (descripcion IS NULL))
+        OR (presentacion IS NOT NULL)
+        OR (descripcion IS NOT NULL)
+        );
+
+--d
+CREATE ASSERTION assertion_d CHECK(
+    NOT EXISTS ( --subquery da los pares en provee donde la suc y el prob esten
+                    --en localidades destintas. Verificamos que no existan esos pares.
+        SELECT nro_prov, cod_suc
+        FROM provee
+        JOIN proveedor ON provee.nro_prov = proveedor.nro_prov
+        JOIN sucursal ON provee.cod_suc = sucursal.cod_suc
+        WHERE sucursal.localidad != proveedor.localidad
+    )
+)
+
+-- Ejercicio 5
+
+--a
+CREATE DOMAIN fecha_nac_valida
+AS date NOT NULL
+CHECK ( extract(year from age(current_date, VALUE)) <= 70 );
+
+--b
+ALTER TABLE voluntario
+ADD CHECK (
+    (nro_voluntario IN (
+            SELECT v1.nro_voluntario --los voluntarios con menos horas que los coord.
+            FROM voluntario v1 --el voluntario
+            JOIN voluntario v2 --el coordinador
+            ON v1.id_coordinador = v2.nro_voluntario AND v1.id_coordinador IS NOT NULL
+            WHERE v1.horas_aportadas <= v2.horas_aportadas
+        )
+    ) OR id_coordinador IS NULL
+    );
+
+--c
+CREATE ASSERTION assertion_c CHECK (
+    NOT EXISTS (
+        SELECT horas_aportadas
+        FROM voluntario v
+        JOIN tarea t ON v.id_tarea = t.id_tarea
+        WHERE v.horas_aportadas
+        NOT BETWEEN t.min_horas AND t.max_horas
+    )
+)
+
+--d
+ALTER TABLE voluntario
+ADD CHECK (
+    (NOT EXISTS(
+            SELECT *
+            FROM voluntario v1 --el voluntario
+            JOIN voluntario v2 --el coordinador
+            ON v1.id_coordinador = v2.nro_voluntario AND v1.id_coordinador IS NOT NULL
+            WHERE v1.id_tarea != v2.id_tarea
+        ))
+    OR v1.id_coordinador IS NULL
+    );
+
+--e
+--este seguro que esta mal
+CREATE ASSERTION CHECK (
+        NOT EXISTS (
+            SELECT nro_voluntario
+            FROM historico
+            GROUP BY nro_voluntario, id_institucion
+            HAVING count(*) > 3
+            AND (extract(year from age(min(fecha_inicio), max(fecha_inicio))) < 1)
+        )
+    );
